c07bddb731d18661008406ad11af8067f46ac4c3
Oliver Stadie <oliver.stadie@gmail.com>
2019-04-23 06:16:52 +0200

    ADD simple respawn on death

---
+++ Assets/units/knight/Health.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Health : MonoBehaviour
+{
+
+    public void takeHit()
+    {
+        die();
+    }
+
+    private void die()
+    {
+        Transform targetIndicator = GetComponent<FollowTransform>().target.transform;
+        targetIndicator.position = Vector3.zero;
+        transform.position = targetIndicator.position;
+    }
+}
---
+++ Assets/units/zombie/HitKnight.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Networking;
+
+public class HitKnight : NetworkBehaviour
+{
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (isServer && collision.gameObject.tag == "Player")
+        {
+            collision.gameObject.GetComponent<Health>().takeHit();
+        }
+    }
+}
