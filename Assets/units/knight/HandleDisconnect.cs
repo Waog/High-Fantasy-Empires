fddcdbb51047b50da5f544887d6e0826693ba8d2
Oliver Stadie <oliver.stadie@gmail.com>
2019-04-23 06:47:21 +0200

    ADD remove disconnected players/knights

---
+++ Assets/units/knight/HandleDisconnect.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+
+public class HandleDisconnect : NetworkBehaviour
+{
+    private void Update()
+    {
+        if (isServer && !GetComponent<FollowTransform>().target)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
